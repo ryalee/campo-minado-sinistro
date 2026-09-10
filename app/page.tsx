7ef1885f205f0e7604946ae5b4c3487b9e2aa951
ryalee <sodre100ryan@gmail>
2026-09-10 16:55:39 -0300

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -31,6 +31,10 @@ export default function MinesweeperPage() {
         <Header />
         <Board />
       </div>
+
+      <footer>
+        <p className="text-white text-lg mt-15">Campo Minado Sinistro | Desenvolvido por <a href="https://ryan-alecrim-portfolio.vercel.app/" target='_blank' className='underline hover:text-amber-100 font-mono'>Ryan Alecrim Sodré</a> | {new Date().getFullYear()} </p>
+      </footer>
     </main>
   );
 }
